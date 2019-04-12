f3b7fede3be622948bd5b726cddf938b6766d31c
Harold <haroldespinoza823@gmail.com>
2019-04-11 19:17:07 -0600

    se sube archivo para probar la nave

---
+++ Pruebas Nave.py
@@ -0,0 +1,12 @@
+#importar la libreria tkinter
+from tkinter import *
+import random
+import tkinter.font
+import tkinter.messagebox
+
+#Crear la ventana, su nombre e icono
+root = Tk()
+root.wm_title("Space Invaders")
+root.iconbitmap("SpIn.ico.ico")
+
+root.mainloop()
