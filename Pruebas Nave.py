c45f5a37c2b0439fd3f49151cbf774be22867f9f
Harold <haroldespinoza823@gmail.com>
2019-04-11 19:36:10 -0600

    actulic¿zacion

--- Pruebas Nave.py
+++ Pruebas Nave.py
@@ -9,4 +9,24 @@ root = Tk()
 root.wm_title("Space Invaders")
 root.iconbitmap("SpIn.ico.ico")
 
+
+
+#se crean canvas
+disp = Canvas(root, width=800, height=800, bg="black")
+
+disp.grid(row=0, column=0)
 root.mainloop()
+shots = []
+enemyProjectiles = []
+aliens = []
+explosions = []
+opAttack = False
+otherOPAttack = False
+wavesSurvived = 0
+dead = False
+hardMode = False
+gOver = tkinter.font.Font(family="Chiller", size=30, weight="bold")
+otherFont = tkinter.font.Font(family="OCR-A II", size=20)
+menuFont = tkinter.font.Font(family="Fixedsys", size=30)
+gameState = 0
+cheatCode = ""
