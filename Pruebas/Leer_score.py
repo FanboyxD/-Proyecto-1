f83f8d35804448872540c90cc94ba92ffd84c304
Harold <haroldespinoza823@gmail.com>
2019-04-17 17:50:13 -0600

    pruebas con json

--- Pruebas/Leer_score.py
+++ Pruebas/Leer_score.py
@@ -1,9 +1,9 @@
 import json
 
-with open('scores_prueba.json') as file:
+with open('puntajes.json') as file:
     scores = json.load(file)
-print(scores["score1"])
-print(scores["score2"])
-print(scores["score3"])
-print(scores["score4"])
-print(scores["score5"])
+print(scores["Nombres"][0])
+print(scores["Nombres"][1])
+print(scores["Nombres"][2])
+print(scores["Nombres"][3])
+print(scores["Nombres"][4])
