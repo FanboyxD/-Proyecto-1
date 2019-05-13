f17a572f77c7f5e2b64164e7eb56c023cb989f7d
Harold <haroldespinoza823@gmail.com>
2019-05-12 22:24:50 -0600

    server update

--- Juego + server/network.py
+++ Juego + server/network.py
@@ -1,23 +1,24 @@
-import socket
-
-
-class Network:
-
-    def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "192.168.1.10" 
-        self.port = 5555    
-        self.addr = (self.host, self.port)
-        self.id = self.connect()
-
-    def connect(self):
-        self.client.connect(self.addr)
-        return self.client.recv(2048).decode()
-
-    def send(self, data):
-        try:
-            self.client.send(str.encode(data))
-            reply = self.client.recv(2048).decode()
-            return reply
-        except socket.error as e:
-            return str(e)
+import socket 
+ 
+ 
+class Network: 
+ 
+    def __init__(self): 
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.ttl = struct.pack('b', 1) 
+        self.client.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)    
+        self.addr = ('224.3.29.71', 5000) 
+ 
+        self.id = self.connect() 
+ 
+    def connect(self): 
+        self.client.connect(self.addr) 
+        return self.client.recv(2048).decode() 
+ 
+    def send(self, data): 
+        try: 
+            self.client.send(str.encode(data)) 
+            reply = self.client.recv(2048).decode() 
+            return reply 
+        except socket.error as e: 
+            return str(e) 
